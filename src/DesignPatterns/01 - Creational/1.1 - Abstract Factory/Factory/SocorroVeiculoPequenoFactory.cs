c5f151cb93a65de28a881f1fc934dc3072899e9a
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-06-09 12:54:46 -0300

    implementando Concrete Factories

---
+++ src/DesignPatterns/01 - Creational/1.1 - Abstract Factory/Factory/SocorroVeiculoPequenoFactory.cs
@@ -0,0 +1,16 @@
+﻿using DesignPatterns._01___Creational._1._1___Abstract_Factory.AbstractFactory;
+
+namespace DesignPatterns._01___Creational._1._1___Abstract_Factory.Factory;
+
+public class SocorroVeiculoPequenoFactory : AutoSocorroFactory
+{
+    public override Guincho CriarGuincho()
+    {
+        return GuinchoCreator.Create(Porte.Pequeno);
+    }
+
+    public override Veiculo CriarVeiculo(string modelo, Porte porte)
+    {
+        return VeiculoCreator.Create(modelo, porte);
+    }
+}
