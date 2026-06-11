6e5e4c350db1f8d42020a43a6d35346f72a08835
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-06-11 11:40:49 -0300

    finalizado Abstract Factory patterns

--- src/DesignPatterns/01 - Creational/1.1 - Abstract Factory/Client/AutoSocorro.cs
+++ src/DesignPatterns/01 - Creational/1.1 - Abstract Factory/Client/AutoSocorro.cs
@@ -1,6 +1,37 @@
-﻿namespace DesignPatterns._01___Creational._1._1___Abstract_Factory.Client;
+﻿using DesignPatterns._01___Creational._1._1___Abstract_Factory.AbstractFactory;
+using DesignPatterns._01___Creational._1._1___Abstract_Factory.Factory;
+
+namespace DesignPatterns._01___Creational._1._1___Abstract_Factory.Client;
 
 public class AutoSocorro
 {
-    
+    private readonly Veiculo _veiculo;
+    private readonly Guincho _guincho;
+
+    public AutoSocorro(AutoSocorroFactory factory, Veiculo veiculo)
+    {
+        _veiculo = factory.CriarVeiculo(veiculo.Modelo, veiculo.Porte);
+        _guincho = factory.CriarGuincho();
+    }
+
+    public void RealizarAtendimento()
+    {
+        _guincho.Socorrer(_veiculo);
+    }
+
+    public static AutoSocorro CriarAutoSocorro(Veiculo veiculo)
+    {
+        switch (veiculo.Porte)
+        {
+            case Porte.Pequeno:
+                return new AutoSocorro(new SocorroVeiculoPequenoFactory(), veiculo);
+            case Porte.Medio:
+                return new AutoSocorro(new SocorroVeiculoMedioFactory(), veiculo);
+            case Porte.Grande:
+                return new AutoSocorro(new SocorroVeiculoGrandeFactory(), veiculo);
+            default:
+                throw new ApplicationException("Não foi possível identificar o veículo.");
+        }
+    }
+
 }
--- src/DesignPatterns/01 - Creational/1.1 - Abstract Factory/Executer/ExecutionAbstractFactory.cs
+++ src/DesignPatterns/01 - Creational/1.1 - Abstract Factory/Executer/ExecutionAbstractFactory.cs
@@ -1,6 +1,20 @@
-﻿namespace DesignPatterns._01___Creational._1._1___Abstract_Factory.Executer;
+﻿using DesignPatterns._01___Creational._1._1___Abstract_Factory.Client;
+
+namespace DesignPatterns._01___Creational._1._1___Abstract_Factory.Executer;
 
 public class ExecutionAbstractFactory
 {
-    
+    public static void Executar()
+    {
+        var veiculosSocorro = new List<Veiculo>
+        {
+            VeiculoCreator.Create("Celta", Porte.Pequeno),
+            VeiculoCreator.Create("Jetta", Porte.Medio),
+            VeiculoCreator.Create("BMW X6", Porte.Grande)
+        };
+        
+        veiculosSocorro.ForEach(v => AutoSocorro
+            .CriarAutoSocorro(v)
+            .RealizarAtendimento());
+    }
 }
