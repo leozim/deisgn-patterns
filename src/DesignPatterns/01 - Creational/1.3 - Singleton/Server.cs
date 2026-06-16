25b6086f1bab7c83aa03f16d79ab218daef7238d
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-06-16 13:15:18 -0300

    implementando padrao sigleton

---
+++ src/DesignPatterns/01 - Creational/1.3 - Singleton/Server.cs
@@ -0,0 +1,8 @@
+﻿namespace DesignPatterns._01___Creational._1._3___Singleton;
+
+public class Server
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string IP { get; set; }
+}
---
+++ src/DesignPatterns/01 - Creational/1.3 - Singleton/Singleton.cs
@@ -0,0 +1,25 @@
+﻿namespace DesignPatterns._01___Creational._1._3___Singleton;
+
+public class Singleton
+{
+    public static void Executar()
+    {
+        var b1 = LoadBalancer.GetInstanceLoadBalancer();
+        var b2 = LoadBalancer.GetInstanceLoadBalancer();
+        var b3 = LoadBalancer.GetInstanceLoadBalancer();
+        var b4 = LoadBalancer.GetInstanceLoadBalancer();
+
+        if (b1 == b2 && b2 == b3 && b3 == b4)
+        {
+            Console.WriteLine("Mesma instância\n");
+        }
+
+        var balancer = LoadBalancer.GetInstanceLoadBalancer();
+
+        for (var i = 0; i < 15; i++)
+        {
+            var serverName = balancer.NextServer.Name;
+            Console.WriteLine("Disparando request para: " + serverName);
+        }
+    }
+}
