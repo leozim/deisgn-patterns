08a78b595b266d29da4c5f31cb5628abbffe6ddd
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-06-24 15:44:07 -0300

    implementando DomainMessage

--- src/DesignPatterns/02 - Structural/2.3 - Composite/CompositeChild/DomainMessage.cs
+++ src/DesignPatterns/02 - Structural/2.3 - Composite/CompositeChild/DomainMessage.cs
@@ -1,6 +1,18 @@
-﻿namespace DesignPatterns._02___Structural._2._3___Composite.CompositeChild;
+﻿using DesignPatterns._02___Structural._2._3___Composite.Interfaces;
 
-public class DomainMessage
+namespace DesignPatterns._02___Structural._2._3___Composite.CompositeChild;
+
+public class DomainMessage : IMessage
 {
+    public string Nome { get; set; }
+
+    public DomainMessage(string nome)
+    {
+        Nome = nome;
+    }
     
+    public void ExibirMessages(int sub)
+    {
+        Console.WriteLine(new string('-', sub) + Nome);
+    }
 }
