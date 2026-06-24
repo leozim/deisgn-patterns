3548f231e1097aa3007f64d8a74a970c0b0dd3c4
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-06-24 15:41:22 -0300

    implementando InputFormMessage

--- src/DesignPatterns/02 - Structural/2.3 - Composite/CompositeChild/InputFormMessage.cs
+++ src/DesignPatterns/02 - Structural/2.3 - Composite/CompositeChild/InputFormMessage.cs
@@ -1,6 +1,17 @@
-﻿namespace DesignPatterns._02___Structural._2._3___Composite.CompositeChild;
+﻿using DesignPatterns._02___Structural._2._3___Composite.Interfaces;
 
-public class InputFormMessage
+namespace DesignPatterns._02___Structural._2._3___Composite.CompositeChild;
+
+public class InputFormMessage : IMessage
 {
-    
+    public string Nome { get; set; }
+
+    public InputFormMessage(string name)
+    {
+        Nome = name;
+    }
+    public void ExibirMessages(int sub)
+    {
+        Console.WriteLine(new string('-', sub) + Nome);
+    }
 }
