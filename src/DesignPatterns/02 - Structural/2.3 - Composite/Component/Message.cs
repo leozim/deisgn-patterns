1a5a3aa65b389b7d1cadeb01a6aa97db20905967
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-06-24 15:39:51 -0300

    implementando classe de message

--- src/DesignPatterns/02 - Structural/2.3 - Composite/Component/Message.cs
+++ src/DesignPatterns/02 - Structural/2.3 - Composite/Component/Message.cs
@@ -1,6 +1,61 @@
-﻿namespace DesignPatterns._02___Structural._2._3___Composite.Component;
+﻿using System.Collections;
+using DesignPatterns._02___Structural._2._3___Composite.Interfaces;
 
-public class Message
+namespace DesignPatterns._02___Structural._2._3___Composite.Component;
+
+public class Message : IMessage, IEnumerable<IMessage>
 {
+    private readonly List<IMessage> _list = new List<IMessage>();
     
+    public string Nome { get; set; }
+
+    public Message(string nome)
+    {
+        Nome = nome;
+    }
+
+    public void AddChild(IMessage child)
+    {
+        _list.Add(child);
+    }
+
+    public void RemoveChild(IMessage child)
+    {
+        _list.Remove(child);
+    }
+
+    public IMessage GetChild(int index)
+    {
+        return _list[index];
+    }
+
+    public IMessage GetChild(string name)
+    {
+        return _list.First(c => c.Nome == name);
+    }
+
+    public IEnumerable<IMessage> GetChilds()
+    {
+        return _list;
+    }
+    
+    public void ExibirMessages(int sub)
+    {
+        Console.WriteLine(new string ('-', sub) + Nome);
+
+        foreach (var message in _list)
+        {
+            message.ExibirMessages(sub + 2);
+        }
+    }
+
+    public IEnumerator<IMessage> GetEnumerator()
+    {
+        return ((IEnumerable<IMessage>)_list).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return ((IEnumerable<IMessage>)_list).GetEnumerator();
+    }
 }
--- src/DesignPatterns/02 - Structural/2.3 - Composite/Interfaces/IMessage.cs
+++ src/DesignPatterns/02 - Structural/2.3 - Composite/Interfaces/IMessage.cs
@@ -1,6 +1,7 @@
 ﻿namespace DesignPatterns._02___Structural._2._3___Composite.Interfaces;
 
 public interface IMessage
-{
-    
+{ 
+    string Nome { get; set; }
+    void ExibirMessages(int sub);
 }
